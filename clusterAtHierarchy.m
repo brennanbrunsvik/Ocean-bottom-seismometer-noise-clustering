e803c62107fbd77721fab0077278354edadd5f92
brennanbrunsvik <brennan.brunsvik@gmail.com>
2021-08-06 14:29:22 -0700

    Made generic function clusterAtHierarchy.m
    which handles the processing at any hierarchical step. Cleaned up cluster_break_instru3_waterOptimize_...

---
+++ clusterAtHierarchy.m
@@ -0,0 +1,72 @@
+function [thissubplot, thisname, thisbool] = clusterAtHierarchy(loopOptimizePenalty, ijk, index, ...
+    thissubplot, pltn, pltm, dataSet, ...
+    dat, fnew, penaltyFunction, showPenalOptim, bools, names, options); 
+    arguments
+            loopOptimizePenalty
+            ijk
+            index
+            thissubplot
+            pltn
+            pltm
+            dataSet
+            dat
+            fnew
+            penaltyFunction
+            showPenalOptim
+            bools
+            names
+            options.thisname = nan
+            options.thisbool = nan
+    end
+%
+depthHier = find(string({'i', 'j', 'k'})==ijk); 
+
+if strcmp(ijk, 'i'); 
+    thisname = names{depthHier}{index}
+	thisbool = logical(ones(size(dat,1), 1)); 
+else
+    thisname = [options.thisname '\newline' names{depthHier}{index}]; 
+    thisbool = options.thisbool; 
+end
+
+if loopOptimizePenalty(depthHier); 
+    if showPenalOptim & (index==1);
+        figure(132); 
+        thisax = subplot(pltn, pltm, thissubplot); 
+        thissubplot = thissubplot + 1; % On Figure 132  
+    else
+        thisax = nan; 
+    end
+    [     eachPenaltyTempTrue,...
+          eachPenaltyTempFalse,...
+          eachPenaltyTempTot, ...
+          eachPenaltyTempTrueN,...
+          eachPenaltyTempFalseN,...
+          eachPenaltyTempTotN,...
+          penBreak...
+          ] = optimize_penalty(dataSet(thisbool), dat(thisbool,:), fnew, penaltyFunction, ...
+              thisax, and(showPenalOptim, (index==1)), 100); 
+      [minPen, ipenBreak] = min(eachPenaltyTempTot); 
+      penBreakBest = penBreak(ipenBreak); 
+      cutInd = penBreakBest; 
+      
+      if index == 1; 
+          thisboolNew = (dataSet  < penBreakBest)'; % 
+      elseif index == 2; 
+          thisboolNew = (dataSet >= penBreakBest)'; % 
+      end
+      
+else; 
+    thisboolNew = bools{depthHier}{index}; % Base level boolean. The dataset corresponding to thisname. 
+end
+
+% thisname = sprintf('%s \n %s - %4.0f m \n', thisname, names{2}{j}, cut2)
+% % TODO need to put this back, with cut2
+thisbool = and(thisboolNew, thisbool); 
+
+% if strcmp('i', ijk); 
+% thisname = names{depthHier}{index}; % Base level name. e.g. "shallow" or "deep"
+% else
+
+
+end
