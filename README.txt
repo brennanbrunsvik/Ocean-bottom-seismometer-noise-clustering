7c187702db7fb82d156af971df7afdf66fa996e8
brennanbrunsvik <brennan.brunsvik@gmail.com>
2022-09-01 18:53:03 -0700

    Some clarification in prep for making public on Github.

---
+++ README.txt
@@ -0,0 +1,5 @@
+This is a minimal working example of the code used for "clustering" analysis in Janiszewski et al. 2022. While you can probably figure out how to use it yourself, I have not edited the code to be self-explanatory. If you would like to use this and find it confusing, feel free to contact me (brennan.brunsvik@gmail.com). Please also email me if you find problems. 
+
+To get started, simply run "clusterLoopBig.m". 
+
+The goal is to seperate ocean-bottom seismometer noise spectra into different groups based on station characteristics (such as depth). Then, evaluate the similarity between spectra in those groups. If a station characteristic produces groups that have highly similar spectral properties, then there may be a strong correlation/causation between noise and that station characteristic.  
--- clusterLoopBig.m
+++ clusterLoopBig.m
@@ -1,17 +1,17 @@
-% clear; 
-% close all; 
-sameStasAllAnalyses = true; 
-showSpectrograms = false; 
-showPenalOptim = logical([0 1 0]); disp('Use this only for 3 layer deep hierarchy thing')
-% showPenalOptim = logical(1); disp('Use this only for 1 layer deep hierarchy thing')
-penaltyFunction = 'spectral_angle'; 
+clear; 
+close all; 
+
+sameStasAllAnalyses = true; % Don't use a station unless it can be used in every analysis. 
+showSpectrograms = false; % Set to false to save some time. 
+penaltyFunction = 'spectral_angle'; % Spectral angle or Euclidean (check code for precise spelling). 
 coh_or_spec = 'spec'; % coherance (coh) or spectra (spec)
+
 addpath('./boot'); 
 addpath('./functionsPlotting'); 
 addpath('./otherPplCode'); 
-
 set(groot, 'defaultAxesFontName', 'helvetica'); 
 
+% Which combination of data, station component, and whether using spectra or coherance. Choose a combination using an integer. I think 1, 5, and 11 are most important. 
 datCompSpec = {...
     {1,1,'spec'},... % 1
     {1,2,'spec'},... 
@@ -26,63 +26,62 @@ datCompSpec = {...
     {1,23,'spec'}... % 11. Both horizontal 1 and 2. 
     };
 
-% labelsAll = {'Water Depth'; 'Plate Bndy Dist'; 'Coastline Dist'; ...
-%             'Crustal Age'; 'Sediment Thickn'; 'Surface Current';...
-%             'OBS Design'; 'Seismometer'; 'Pressure Gauge'; 'Environment'; 'Experiment'}; % Should be able to remove this from here, but I was using it for size(...)
+% Should be able to remove this from here, but I was using it for size(...)
 labelsAll = {'Water Depth','Distance to Plate Boundary','Distance from Land',...
     'Crustal Age','Sediment Thickness','Surface Current','Instrument Design',...
 	'Seismometer','Pressure Gauge','Environment','Experiment Name'}; % Applies to each quant
-save('labelsAll', 'labelsAll'); 
+save('labelsAll', 'labelsAll'); % For loading in some functions, I think. 
         
         
-% This chooses which analysis to do. Loop through all analyses, or just do
-% some. 
-eachLayerDepth = [3]; % [1,3]; 
+% This chooses which analysis to do. Loop through all analyses, or just do some. 
+eachLayerDepth = [3]; % [1,3]; % Refers to the number of layers/hierarchies
 % eachDatComp = [1:size(datCompSpec,2)]; 
-eachDatComp = [1]; % [1, 5, 11]; 
-eachQuant = [3]; % [1:length(labelsAll)];  
+eachDatComp = [1]; % [1, 5, 11]; % Refers to datCompSpec
+eachQuant = [3]; % [1:length(labelsAll)]; % Refers to which quality to assess. Pick any from LabelsAll.   
 % eachQuant = 1; 
-savePenaltFile = false; 
-% warning('You are overwriting penalty files for plotting!')
+savePenaltFile = false; if savePenaltFile; warning('You are overwriting penalty files that are used for plotting!'); end % Save penalty calculation files. At some point you have to do this, and loop through all hierarchy depths and labels, so that we can make plots of all those results. 
 
-% parpool(8); 
+
         
 for iLayerDepth = eachLayerDepth; % Do layer depths seperately. Easiest coding solution, since single or multiple layer analyses used the same figure windows, and were originally intended to run seperately. 
 for idatcomp = eachDatComp; % Loop through all combinations of datswitcs, seismometer component, and spectra/coherance. 
-disp(sprintf('layer depth: %2.0f, datcomp: %2.0f', iLayerDepth, idatcomp))
+    
+fprintf('Running layer depth: %2.0f, datcomp: %2.0f\n', iLayerDepth, idatcomp)
+
 thisDatCompSpec = datCompSpec{idatcomp}; 
 datswitch   = thisDatCompSpec{1}; 
 component   = thisDatCompSpec{2}; 
 coh_or_spec = thisDatCompSpec{3}; 
 
-%%% Disp some info...
-finPenFile = sprintf('pen_results/penalties_%s_layerDepth%1.0f_datswitch%1.0f_component%1.0f.mat',...
-    coh_or_spec, iLayerDepth, datswitch, component) 
-% end
-% end
-%%% End disp some info
+% finPenFile = sprintf('pen_results/penalties_%s_layerDepth%1.0f_datswitch%1.0f_component%1.0f.mat',...
+%     coh_or_spec, iLayerDepth, datswitch, component) % I think store penalty information in this file, for the specific datcomp/layerdepth. 
 
 eachPenalty      = zeros(size(labelsAll,1), 1); % Penalty to collect while doing the iquant loop
-eachPenalty2Deep = nan  (size(labelsAll,1), 1); % Auxiliary info, but used for plotting later. only care if doing 3 layer hierarchy. 
+eachPenalty2Deep = nan  (size(labelsAll,1), 1); % Auxiliary info, but used for plotting later. Only care if doing 3 layer hierarchy. 
 eachPenalty1Deep = nan  (size(labelsAll,1), 1); 
-penaltyUnClust   = nan  (size(labelsAll,1), 1); % only need this once, but this works in the parfor...
-% for iquant = eachQuant; % If not doing plots, switch to parfor
-for iquant = eachQuant; % Loop through each explanatory/independent variable ("quant" is here for legacy purpose basically) % Slower to put this loop so high up in code, but makes it easier to think and develop the code. 
-% !!! Do all the real processing at this section.
+penaltyUnClust   = nan  (size(labelsAll,1), 1); % Penalty of data before clustering any of it. only need this once, but this works in the parfor... 
 
-    if iLayerDepth == 1; 
+% parfor iquant = eachQuant; % If not doing plots, switch to parfor
+for iquant = eachQuant; % Loop through each explanatory/independent variable ("quant" is here for legacy purpose basically) % Slower to put this loop so high up in code, but makes it easier to think and develop the code. 
+
+    
+    %%% Wrap code for all the processing. 
+    if iLayerDepth == 1; % If doing only one layer. 
+        showPenalOptim = logical(1); 
+        
         [eachPenalty(iquant), penaltyUnClust(iquant)] ...
             = cluster1LayerHierarchy(sameStasAllAnalyses, ...
             showSpectrograms, showPenalOptim, penaltyFunction, ...
-            coh_or_spec, datswitch, component, iquant);  % Single layer deep
-    elseif iLayerDepth == 3; 
+            coh_or_spec, datswitch, component, iquant); 
+    elseif iLayerDepth == 3; % If doing three layers. 
+        showPenalOptim = logical([0 0 0]); % Which layers to plot penalties of
+        
         [eachPenalty(iquant), eachPenalty2Deep(iquant), eachPenalty1Deep(iquant), penaltyUnClust(iquant)] ...
             = clusterWholeHierarchy(sameStasAllAnalyses, ...
             showSpectrograms, showPenalOptim, penaltyFunction, ...
-            coh_or_spec, datswitch, component, iquant);   % Three layers deep
-        % Not really a need to get penaltyUnClust each time. But this was easiest
-        % to code. 
+            coh_or_spec, datswitch, component, iquant);   
     end
+    %%% END Wrap code for all the processing. 
 
 end
 
@@ -91,29 +90,14 @@ finPenData = struct('eachPenalty', eachPenalty', ...
                     'eachPenalty3Deep', eachPenalty1Deep', ...
                     'labelsAll', labelsAll', ...
                     'penaltyUnClust', penaltyUnClust'); % Add other things later. 
+                
 finPenFile = sprintf('pen_results/penalties_%s_layerDepth%1.0f_datswitch%1.0f_component%1.0f.mat',...
     coh_or_spec, iLayerDepth, datswitch, component); 
+
 if savePenaltFile; 
     save(finPenFile, 'finPenData');
 end
-% disp('NOT SAVING FINPENFILE RIGHT NOW')
 
-% % Can plot histogram of final penalty, based on each third hierarchy depth
-% figure(70); clf; hold on; 
-% barh(eachPenalty)
-% 
-% set(gca, 'yticklabel', labelsAll)
-% for itxt = [1:length(labelsAll)]; 
-%     text(0, itxt, labelsAll{itxt}); 
-% end
-% 
-% xlabel('Penalty using this variable as SOME hierarchy depth (First or third? TODO)'); 
-% set(gca, 'ytick', []); 
-% 
-% box on; 
-% grid on; 
-% 
-% exportgraphics(gcf, 'FIGURES/bar_single_analysis.pdf'); 
 
 end
 end
